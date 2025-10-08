18adf545c08a68869392c286dcdd06e2dd5f9345
xjm <alibi_jia@foxmail.com>
2025-10-08 19:27:33 +0800

    add headless ui

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,7 @@
 import { Head } from "fresh/runtime";
 import { define } from "tools/utils.ts";
 import { FaGithub, FaSignOutAlt } from "react-icons/fa";
+import { Button } from "@headlessui/react";
 export default define.page(function Home() {
   return (
     <div class="px-4 py-8 mx-auto fresh-gradient min-h-screen">
@@ -16,6 +17,9 @@ export default define.page(function Home() {
       >
         <FaGithub size={24} />
       </a>
+      <Button className="rounded bg-sky-600 px-4 py-2 text-sm text-white data-hover:bg-sky-500 data-hover:data-active:bg-sky-700">
+        日内瓦
+      </Button>
       <a
         title="Sign out"
         href="/api/oauth/signout"
