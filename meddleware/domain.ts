629e2cc3e1f17debd30b8ac45e19d2cd4e5494e6
alibi <alibi_jia@outlook.com>
2026-01-10 19:49:40 +0800

    fix: 域名重定向

--- meddleware/domain.ts
+++ meddleware/domain.ts
@@ -3,7 +3,7 @@ import { define } from "tools/utils.ts";
 export const domainMiddleware = define.middleware((ctx) => {
   console.log("host is", new URL(ctx.req.url).host);
   if (new URL(ctx.req.url).host.includes("fresh.lucardo.website")) {
-    return Response.redirect("https://fresh.luchador.dev", 301);
+    return ctx.redirect("https://fresh.luchador.dev", 301);
   }
   return ctx.next();
 });
